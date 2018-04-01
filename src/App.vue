a4481e24473aeef6e6bc4dd7c6aa2b5cdd7845c8
Julien Granier <julien.granier73@gmail.com>
2018-04-01 13:06:21 +0200

    Ajout du navigation drawer (maj des données locales)

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <template>
     <v-app>
+        <app-drawer :drawer.sync="drawer"></app-drawer>
+        <v-toolbar color="indigo" dark fixed app>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Pk Area</v-toolbar-title>
+        </v-toolbar>
         <v-content>
             <router-view></router-view>
         </v-content>
     </v-app>
 </template>
 <script>
+import AppDrawer from './components/AppDrawer';
+
 export default {
     data() {
         return {
-           
+            drawer: false
         }
     },
+
     methods: {
+    },
+
+    components: {
+        AppDrawer
     }
 }
 
---
+++ src/components/AppDrawer.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-navigation-drawer fixed v-model="mutatedDrawer" app>
+        <v-list subheader>
+            <v-subheader inset>Pk Area</v-subheader>
+            <v-list-tile @click="updatePks">
+                <v-list-tile-action>
+                    <v-icon>refresh</v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                    <v-list-tile-title>Mettre à jour les pks</v-list-tile-title>
+                </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+
+    props: {
+        drawer:{
+            type: Boolean,
+            required: true
+        }
+    },
+
+    computed: {
+        mutatedDrawer:{
+            get(){
+                return this.drawer;
+            },
+
+            set(v){
+                this.$emit('update:drawer', v)
+            }
+        }
+    },
+
+    methods: {
+        updatePks(){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
+
